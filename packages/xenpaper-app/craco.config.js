7600ae2ff2db1ed4467f123866d3cc8d5527d5f5
Damien Clarke <dclarke@atlassian.com>
2025-01-30 11:14:17 +1100

    ugh

--- packages/xenpaper-app/craco.config.js
+++ packages/xenpaper-app/craco.config.js
@@ -1,18 +1,21 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  webpack: {
-    alias: {
-      react: path.resolve(__dirname, '../../node_modules/react'),
+    webpack: {
+        alias: {
+            react: path.resolve(__dirname, "../../node_modules/react"),
+        },
+        configure: (webpackConfig, { env, paths }) => {
+            webpackConfig.module.rules[1].oneOf[2].include = [
+                path.resolve(__dirname, "./src"),
+                path.resolve(__dirname, "../xenpaper-ui/src"),
+                path.resolve(__dirname, "../mosc/src"),
+                path.resolve(__dirname, "../sound-engine-tonejs/src"),
+            ];
+            return webpackConfig;
+        },
     },
-    configure: (webpackConfig, { env, paths }) => {
-        webpackConfig.module.rules[1].oneOf[2].include = [
-            path.resolve(__dirname, './src'),
-            path.resolve(__dirname, '../xenpaper-ui/src'),
-            path.resolve(__dirname, '../mosc/src'),
-            path.resolve(__dirname, '../sound-engine-tonejs/src')
-        ];
-        return webpackConfig;
-    }
-  }
+    eslint: {
+        enable: false,
+    },
 };
